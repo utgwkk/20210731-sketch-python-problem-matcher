b342ec0faa35d59299cc84937354324c30657412
utgwkk <utagawakiki@gmail.com>
2021-07-31 13:14:06 +0900

    move files

--- syntax_test.py
+++ syntax_test.py
@@ -2,7 +2,7 @@ from glob import glob
 import py_compile
 import pytest
 
-python_files = glob('*.py', recursive=True)
+python_files = glob('src/**/*.py', recursive=True)
 
 @pytest.mark.parametrize("path", python_files)
 def test_syntax(path):
